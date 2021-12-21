cc89c4667dd435196a3a3e16c96128e56f110f07
kevinSarli <kevin.sarli99@gmail.com>
2021-12-21 01:09:14 -0300

    modelo

---
+++ src/database/config/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": 'oliver',
+    "database": "movies_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": 'oliver',
+    "database": "movies_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
