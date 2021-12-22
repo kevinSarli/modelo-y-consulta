2f2c65add234580a5200ffb54734a3bc9b163fcc
kevinSarli <kevin.sarli99@gmail.com>
2021-12-22 17:56:18 -0300

    cambios en rutas

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {list,nueva,recommended,detail,add,create,edit,update,destroy,remove} = require('../controllers/moviesController');
-const addMovieValidator = require('../validations/addMovieValidator');
+const {list,nueva,recommended,detail} = require('../controllers/moviesController');
 
 
 router
@@ -10,11 +9,6 @@ router
     .get('/movies/recommended', recommended)
     .get('/movies/detail/:id', detail)
 
-    .get('/add', add)
-    .post('/add', addMovieValidator, create)
-    .get('/edit/:id', edit)
-    .put('/edit/:id',addMovieValidator, update)
-    .get('/delete/:id', remove)
-    .delete('/delete/:id', destroy)
+    
 
 module.exports = router;
